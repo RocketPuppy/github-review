df3ab051e199d9d0e9f9e1d503f75a4779571ff1
Daniel Wilson <daniel@mojotech.com>
2015-03-20 21:32:44 -0400

    Display commit shas with vty-ui

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,9 @@ import qualified Github.Data.Definitions as GDD
 import Github.Auth
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text as T
+import Graphics.Vty.Widgets.All
+import Graphics.Vty.Input.Events
 
 data CommandArgs = CommandArgs { usernameArg :: String
                                , passwordFlag :: Bool
@@ -38,7 +41,38 @@ main = do
         Left _ -> error "Could not find PR with that ID"
         Right pr -> case prCommits of
             Left _ -> error "Could not get commits for PR"
-            Right commits -> putStrLn (show pr) (show commits)
+            Right commits -> doReview pr commits
+
+doReview :: DetailedPullRequest -> [GDD.Commit] -> IO ()
+doReview pr commits = do
+    -- Commits listing
+    let title = "PR: #" ++ (show . detailedPullRequestNumber $ pr)
+        head = pullRequestCommitSha . detailedPullRequestHead $ pr
+        base = pullRequestCommitSha . detailedPullRequestBase $ pr
+
+    title <- plainText . T.pack $ title
+    commitsShas <- flip newTextList 1 . reverse . map (T.pack . commitSha) $ commits
+
+    commitsPane <- return title <--> hBorder <--> return commitsShas
+
+    commitView <- newTextList [] 1
+
+    commits <- return commitsPane <++> vBorder <++> return commitView
+
+    fg <- newFocusGroup
+    addToFocusGroup fg commitsShas
+    addToFocusGroup fg commitView
+
+    c <- newCollection
+    addToCollection c commits fg
+
+    commitView <- plainText "some text"
+    fg `onKeyPressed` \_ key _ ->
+        if key == KChar 'q'
+            then shutdownUi >> return True
+            else return False
+
+    runUi c defaultContext
 
 -- SO: http://stackoverflow.com/questions/4064378/prompting-for-a-password-in-haskell-command-line-application
 getPassword :: IO String
